f74cd5efd7f1864aaf06eb1380bd1037345d4f7f
swatidurgekar <swatidurgekar@gmail.com>
2024-08-08 16:48:04 +0530

    fix: CORS issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default {
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://3.223.98.72:1337",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+};
