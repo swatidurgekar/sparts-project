72d36e472a2e3016255d4c5b1d7c8b023609bd20
swatidurgekar <swatidurgekar@gmail.com>
2024-08-08 16:51:50 +0530

    update vit config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-export default {
+export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
         target: "http://3.223.98.72:1337",
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-};
+});
